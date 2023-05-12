2cb3e9b4cccc2aabf972be4cc2808544cef6594d
r4ha <erbees.50000@gmail.com>
2023-05-12 22:51:48 +0700

    updates config

--- config/config.go
+++ config/config.go
@@ -31,7 +31,7 @@ func InitDB() {
 		DB_Username: "root",
 		DB_Password: "password",
 		DB_Port:     "3306",
-		DB_Host:     "localhost",
+		DB_Host:     "go-mini-project.cjlpgrpxfahb.ap-southeast-1.rds.amazonaws.com",
 		DB_Name:     "gominiproject",
 	}
 
